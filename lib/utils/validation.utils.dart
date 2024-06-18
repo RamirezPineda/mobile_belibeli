a5bdd9266165d16aaff4251ceafbb763f2b950ce
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-17 21:42:34 -0400

    feat: add validations utils

---
+++ lib/utils/validation.utils.dart
@@ -0,0 +1,47 @@
+class TValidator {
+  static String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Email is required.';
+    }
+
+    // Regular expression for email validation
+    final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    // r'^(([^&lt;&gt;()[\]\\.,;:\s@\"]+(\.[^&lt;&gt;()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
+
+    if (!emailRegExp.hasMatch(value)) {
+      return 'Invalid email address.';
+    }
+
+    return null;
+  }
+
+  static String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Password is required.';
+    }
+
+    // Check for minimum password length
+    if (value.length < 8) {
+      return 'Password must be at least 8 characters long.';
+    }
+
+    // Check for uppercase letters
+    // if (!value.contains(RegExp(r'[A-Z]'))) {
+    //   return 'Password must contain at least one uppercase letter.';
+    // }
+
+    // Check for numbers
+    // if (!value.contains(RegExp(r'[0-9]'))) {
+    //   return 'Password must contain at least one number.';
+    // }
+
+    // Check for special characters
+    // if (!value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+    //   return 'Password must contain at least one special character.';
+    // }
+
+    // r"^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$"
+
+    return null;
+  }
+}
