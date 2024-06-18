f73a25f4f7f3b13aa1b20c6a85eeebc80a59c17b
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-17 21:45:14 -0400

    feat: add routes

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,11 +1,16 @@
-import 'package:app_belibeli/screens/login/login_screen.dart';
+import 'package:app_belibeli/screens/login/login.screen.dart';
 import 'package:go_router/go_router.dart';
 
 class Routes {
+  static const root = '/';
   static const login = '/login';
 
   static GoRouter getRoutes() {
     return GoRouter(routes: [
+      GoRoute(
+        path: Routes.root,
+        builder: (context, state) => const LoginScreen(),
+      ),
       GoRoute(
         path: Routes.login,
         builder: (context, state) => const LoginScreen(),
