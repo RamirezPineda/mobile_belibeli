eeb0f3f3fae7199cf7ce49f1ae7d743133f3bdf4
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-17 21:36:40 -0400

    feat: add loading widget

---
+++ lib/widgets/loading.wiget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Loading extends StatelessWidget {
+  const Loading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 20,
+      height: 20,
+      child: CircularProgressIndicator(color: Colors.white, strokeWidth: 4),
+    );
+  }
+}
