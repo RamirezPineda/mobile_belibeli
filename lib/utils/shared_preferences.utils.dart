90f088f00cfab2f0f5c9d4baee04f58b86bb2d46
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-21 20:04:04 -0400

    feat: add shared preferences

---
+++ lib/utils/shared_preferences.utils.dart
@@ -0,0 +1,48 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalStorage {
+  static const token = 'token';
+  static const id = 'id';
+  static const name = 'name';
+  static const email = 'email';
+}
+
+class UserPreferences {
+  static final UserPreferences _instance = UserPreferences._internal();
+  late SharedPreferences _prefs;
+
+  factory UserPreferences() => _instance;
+
+  UserPreferences._internal();
+
+  initPrefs() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
+
+  get prefsUser => _prefs;
+
+  String get token => _prefs.getString(LocalStorage.token) ?? '';
+  String get id => _prefs.getString(LocalStorage.id) ?? '';
+  String get email => _prefs.getString(LocalStorage.email) ?? '';
+  String get name => _prefs.getString(LocalStorage.name) ?? '';
+
+  set token(String token) => _prefs.setString(LocalStorage.token, token);
+  set id(String id) => _prefs.setString(LocalStorage.id, id);
+  set email(String email) => _prefs.setString(LocalStorage.email, email);
+  set name(String name) => _prefs.setString(LocalStorage.name, name);
+
+  static void saveUserPreferences(Map<String, dynamic> dataMap) {
+    final prefs = UserPreferences();
+    prefs.token = dataMap[LocalStorage.token];
+    prefs.id = dataMap[LocalStorage.id];
+    prefs.email = dataMap[LocalStorage.email];
+    prefs.name = dataMap[LocalStorage.name];
+  }
+
+  void clearUser() {
+    _prefs.setString(LocalStorage.token, '');
+    _prefs.setString(LocalStorage.id, '');
+    _prefs.setString(LocalStorage.email, '');
+    _prefs.setString(LocalStorage.name, '');
+  }
+}
