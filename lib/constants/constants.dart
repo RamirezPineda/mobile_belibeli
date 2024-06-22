f37dde6b371311f0d1a910800b7aab50c4df00b4
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-21 20:02:31 -0400

    feat: add environment constants

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,2 +1,3 @@
+export 'package:app_belibeli/constants/environment.constants.dart';
 export 'package:app_belibeli/constants/endpoints.constants.dart';
 export 'package:app_belibeli/constants/colors.constants.dart';
