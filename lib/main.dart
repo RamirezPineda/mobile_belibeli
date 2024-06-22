91b6e1e42470160dc0ac8cc7b50b6b9ffff197a1
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-21 20:03:27 -0400

    fear: add environments

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
-import 'package:app_belibeli/routes/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() => runApp(const MyApp());
+import 'package:app_belibeli/routes/app_router.dart';
+import 'package:app_belibeli/utils/utils.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final userPreferences = UserPreferences();
+  await userPreferences.initPrefs();
+
+  await dotenv.load(fileName: '.env');
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,6 +24,10 @@ class MyApp extends StatelessWidget {
       title: 'BeliBeli.com',
       debugShowCheckedModeBanner: false,
       routerConfig: Routes.getRoutes(),
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+        useMaterial3: true,
+      ),
     );
   }
 }
