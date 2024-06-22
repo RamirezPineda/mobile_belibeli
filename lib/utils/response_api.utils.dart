21d4d239de4bcc8c9ae4acc78c680098c088348d
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-21 20:04:28 -0400

    feat: add response api utils

---
+++ lib/utils/response_api.utils.dart
@@ -0,0 +1,11 @@
+class ResponseApi {
+  static const String data = 'data';
+  static const String statusCode = 'statusCode';
+  static const String countData = 'countData';
+}
+
+class ResponseError {
+  static const String messages = 'messages';
+  static const String statusCode = 'statusCode';
+  static const String error = 'error';
+}
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,4 @@
-export 'package:app_belibeli/utils/response_error.utils.dart';
+export 'package:app_belibeli/utils/shared_preferences.utils.dart';
+export 'package:app_belibeli/utils/response_api.utils.dart';
 export 'package:app_belibeli/utils/http_client.utils.dart';
 export 'package:app_belibeli/utils/validation.utils.dart';
