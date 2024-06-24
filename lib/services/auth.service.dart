1fd3d08f583b611f6ee55be0e4ca72a631929caf
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-23 23:02:04 -0400

    feat: add user data save functon

--- lib/services/auth.service.dart
+++ lib/services/auth.service.dart
@@ -4,6 +4,19 @@ import 'package:app_belibeli/utils/utils.dart';
 
 class AuthService {
   Future<Map<String, dynamic>> login(Auth auth) async {
-    return await HttClient.post(Endpoints.login, auth.toMap());
+    Map<String, dynamic> response =
+        await HttClient.post(Endpoints.login, auth.toMap());
+    final data = response[ResponseApi.data];
+
+    if (data != null) {
+      final data = response[ResponseApi.data];
+
+      data[LocalStorage.token] = response[LocalStorage.token];
+      UserPreferences.saveUserPreferences(data);
+
+      return data;
+    } else {
+      return response;
+    }
   }
 }
