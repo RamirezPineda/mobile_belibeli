1b527c8d06ef55520e9217d83e2c8de78c2e3ce8
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-23 23:14:48 -0400

    feat: add init screen

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,1 +1,7 @@
+export 'package:app_belibeli/screens/profile/profile.screen.dart';
+export 'package:app_belibeli/screens/product/product.screen.dart';
+export 'package:app_belibeli/screens/favorites/favorites.screen.dart';
+export 'package:app_belibeli/screens/shopping-cart/shopping_cart.screen.dart';
+export 'package:app_belibeli/screens/home/home.screen.dart';
+export 'package:app_belibeli/screens/init.screen.dart';
 export 'package:app_belibeli/screens/login/login.screen.dart';
