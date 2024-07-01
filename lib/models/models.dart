a2fc6279f610d6511b6589e3a0222ed73bf9917f
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-30 22:35:33 -0400

    feat: add product-favorite model

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,3 +1,8 @@
+export 'package:app_belibeli/models/product_favorite.model.dart';
+export 'package:app_belibeli/models/product.model.dart';
+export 'package:app_belibeli/models/discount.model.dart';
+export 'package:app_belibeli/models/package.model.dart';
+export 'package:app_belibeli/models/product_image.model.dart';
 export 'package:app_belibeli/models/category.model.dart';
 export 'package:app_belibeli/models/auth.model.dart';
 export 'package:app_belibeli/models/user.model.dart';
