70adc981f82c5d0e7f178978e5409d6b64a108cf
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-30 22:41:18 -0400

    feat: add products and products-favorites endpoints

--- lib/constants/endpoints.constants.dart
+++ lib/constants/endpoints.constants.dart
@@ -2,4 +2,6 @@ class Endpoints {
   static const login = '/auth/login';
   static const singUp = '/auth/register';
   static const categories = '/categories';
+  static const products = '/products';
+  static const productsFavorites = '/products-favorite';
 }
