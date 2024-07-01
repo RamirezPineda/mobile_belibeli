f4345b745a0e594df8919393fe018fd1f9844d7c
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-07-01 10:22:34 -0400

    feat: add categories widget

---
+++ lib/widgets/categories.widget.dart
@@ -0,0 +1,60 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:app_belibeli/models/models.dart';
+
+class Categories extends StatelessWidget {
+  final List<Category> categories;
+  const Categories({super.key, required this.categories});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 70,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(25),
+                    child: Container(
+                      color: Colors.grey.shade300,
+                      padding: const EdgeInsets.all(2),
+                      child: CachedNetworkImage(
+                        imageUrl: categories[index].imageUrl,
+                        placeholder: (context, url) =>
+                            const CircularProgressIndicator(),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
+                        fit: BoxFit.cover,
+                        height: 45,
+                        width: 45,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    categories[index].name,
+                    style: TextStyle(
+                      fontSize: 12.5,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey.shade500,
+                      //  color: categories[index].id
+                      //     ? Colors.grey.shade900
+                      //     : Colors.grey.shade600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
