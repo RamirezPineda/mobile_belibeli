eb59ca1dad790fbf7a351d728a64596ceff78bdd
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-06-30 22:43:26 -0400

    feat: add product-card widget

--- lib/screens/favorites/favorites.screen.dart
+++ lib/screens/favorites/favorites.screen.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
+
+import 'package:app_belibeli/routes/app_router.dart';
+import 'package:app_belibeli/providers/providers.dart';
+import 'package:app_belibeli/widgets/widgets.dart';
+
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final productsFavoritesProvider = context.watch<ProductFavoriteProvider>();
+    final products = productsFavoritesProvider.productsFavorite;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Favorites'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: GridView.builder(
+            shrinkWrap: true,
+            physics: const ScrollPhysics(),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              childAspectRatio: 10 / 16,
+              crossAxisCount: 2,
+              mainAxisSpacing: 6,
+              crossAxisSpacing: 6,
+            ),
+            itemCount: products.length,
+            itemBuilder: (_, index) => ProductCard(
+              product: products[index],
+              isFavorite: true,
+              onTap: () =>
+                  context.push('${Routes.product}/${products[index].id}'),
+            ),
+          ),
+        ),
       ),
     );
   }
