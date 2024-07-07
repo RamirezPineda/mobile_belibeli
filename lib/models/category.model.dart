8243ecc9b8feaa5c89b093ff29c315600430bfb8
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-07-06 23:51:23 -0400

    feat: add product attribute

--- lib/models/category.model.dart
+++ lib/models/category.model.dart
@@ -1,16 +1,20 @@
 import 'dart:convert';
 
+import 'package:app_belibeli/models/models.dart';
+
 class Category {
   String id;
   String name;
   String imageUrl;
   String? description;
+  List<Product>? product;
 
   Category({
     required this.id,
     required this.name,
     required this.imageUrl,
     this.description,
+    this.product,
   });
 
   String toJson() => json.encode(toMap());
@@ -20,6 +24,7 @@ class Category {
         "name": name,
         "imageUrl": imageUrl,
         "description": description,
+        "product": product,
       };
 
   factory Category.fromMap(Map<String, dynamic> json) => Category(
@@ -27,5 +32,9 @@ class Category {
         name: json["name"],
         imageUrl: json['imageUrl'],
         description: json["description"],
+        product: json["product"] != null
+            ? List<Product>.from(
+                json['product'].map((prodJson) => Product.fromMap(prodJson)))
+            : null,
       );
 }
