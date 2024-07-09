f81c6e3c1d5472527fc77f1251015933dd3e4fad
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-07-08 23:45:49 -0400

    feat: add stripe-public-key environment

--- lib/constants/environment.constants.dart
+++ lib/constants/environment.constants.dart
@@ -2,4 +2,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
   static final apiUrl = dotenv.env['API_URL'] ?? 'http://localhost:4000';
+  static final stripePublicKey =
+      dotenv.env['STRIPE_PUBLIC_KEY'] ?? 'your_public_ley';
 }
