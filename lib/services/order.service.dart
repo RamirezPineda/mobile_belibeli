aeec91932a8c78c69861e26bd98a2a922c35ee12
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-07-09 19:16:44 -0400

    refactor: refactor order service

--- lib/services/order.service.dart
+++ lib/services/order.service.dart
@@ -3,17 +3,43 @@ import 'package:app_belibeli/models/models.dart';
 import 'package:app_belibeli/utils/utils.dart';
 
 class OrderService {
-  Future<Map<String, dynamic>> createPayment(CreateOrder dataPayment) async {
+  Future<Map<String, dynamic>> createPayment(
+      List<ShoppingCart> shoppingCart) async {
+    List<CreateProductOrder> productOrder = [];
+
+    for (var element in shoppingCart) {
+      productOrder.add(CreateProductOrder(
+          quantity: element.quantity, productId: element.product.id));
+    }
+
+    final orderData = CreateOrder(note: 'Payment', productOrder: productOrder);
+
     Map<String, dynamic> response =
-        await HttClient.post(Endpoints.createPayment, dataPayment.toMap());
+        await HttClient.post(Endpoints.createPayment, orderData.toMap());
     final data = response[ResponseApi.data];
+
     return data ?? {};
   }
 
-  Future<Map<String, dynamic>> createOrderPayment(CreateOrder dataOrder) async {
+  Future<Map<String, dynamic>> createOrderPayment(
+      List<ShoppingCart> shoppingCart) async {
+    List<CreateProductOrder> productOrder = [];
+
+    for (var element in shoppingCart) {
+      productOrder.add(CreateProductOrder(
+          quantity: element.quantity, productId: element.product.id));
+    }
+
+    final orderData = CreateOrder(
+      note:
+          'Thank you for choosing us for your purchase! We hope you enjoy your product. Please feel free to contact our customer support team if you need additional help or have any questions.',
+      productOrder: productOrder,
+    );
+
     Map<String, dynamic> response =
-        await HttClient.post(Endpoints.createPayment, dataOrder.toMap());
+        await HttClient.post(Endpoints.createOrderPayment, orderData.toMap());
     final data = response[ResponseApi.data];
+
     return data ?? {};
   }
 }
