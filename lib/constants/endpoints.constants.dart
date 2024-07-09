d014764cbcb6addb4bbb83ffd1ff1d0da0e67fed
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-07-08 23:44:40 -0400

    feat: add payment endpoints

--- lib/constants/endpoints.constants.dart
+++ lib/constants/endpoints.constants.dart
@@ -4,4 +4,6 @@ class Endpoints {
   static const categories = '/categories';
   static const products = '/products';
   static const productsFavorites = '/products-favorite';
+  static const createPayment = '/create-payment';
+  static const createOrderPayment = '/create-order-payment';
 }
