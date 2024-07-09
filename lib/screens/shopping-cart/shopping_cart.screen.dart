38410d490c22c3f8537af5174a064b58a1bf8a2c
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-07-09 19:19:34 -0400

    feat: add checkout

--- lib/screens/shopping-cart/shopping_cart.screen.dart
+++ lib/screens/shopping-cart/shopping_cart.screen.dart
@@ -2,18 +2,27 @@ import 'package:flutter/material.dart';
 
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 import 'package:app_belibeli/routes/app_router.dart';
 import 'package:app_belibeli/providers/providers.dart';
-import 'package:app_belibeli/utils/utils.dart';
 
-class ShoppingCartScreen extends StatelessWidget {
+import 'package:app_belibeli/screens/shopping-cart/widgets/item_card.dart';
+import 'package:app_belibeli/screens/shopping-cart/widgets/text_card.dart';
+
+class ShoppingCartScreen extends StatefulWidget {
   const ShoppingCartScreen({super.key});
 
   @override
+  State<ShoppingCartScreen> createState() => _ShoppingCartScreenState();
+}
+
+class _ShoppingCartScreenState extends State<ShoppingCartScreen> {
+  @override
   Widget build(BuildContext context) {
     final shoppingCartProvider = context.watch<ShoppingCartProvider>();
+    final orderProvider = context.watch<OrderProvider>();
+
     final shoppingCartList = shoppingCartProvider.shoppingCart;
 
     return Scaffold(
@@ -28,223 +37,80 @@ class ShoppingCartScreen extends StatelessWidget {
               itemCount: shoppingCartList.length,
               itemBuilder: (context, index) {
                 final shoppingCart = shoppingCartList[index];
-                return Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
-                  child: Row(
-                    children: [
-                      // image
-                      InkWell(
-                        onTap: () {
-                          context.push(
-                              '${Routes.product}/${shoppingCart.product.id}');
-                        },
-                        child: Hero(
-                          tag: shoppingCart.product.id,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(5),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.grey[200],
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              child: CachedNetworkImage(
-                                imageUrl:
-                                    shoppingCart.product.productImage[0].url,
-                                placeholder: (context, url) =>
-                                    const CircularProgressIndicator(),
-                                errorWidget: (context, url, error) =>
-                                    const Icon(Icons.error),
-                                height: 60,
-                                width: 60,
-                                fit: BoxFit.contain,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 12),
-
-                      // name
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            width: 190,
-                            child: Text(
-                              shoppingCart.product.name,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 2,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 5),
-                          Text(
-                            "\$${(TCalculate.calculatePrice(
-                                  price: shoppingCart.product.price,
-                                  tax: shoppingCart.product.tax,
-                                  discount:
-                                      shoppingCart.product.discount?.amount,
-                                ) * shoppingCart.quantity).toStringAsFixed(2)}",
-                            style: const TextStyle(
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(width: 5),
-                      Row(
-                        children: [
-                          InkWell(
-                            onTap: () {
-                              shoppingCartProvider
-                                  .reduceCartQuantity(shoppingCart.product.id);
-                            },
-                            borderRadius: BorderRadius.circular(10.0),
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 10.0, vertical: 4.0),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10.0),
-                                color: Colors.grey.shade100,
-                              ),
-                              child: Text(
-                                '-',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 18,
-                                  color: Colors.grey.shade800,
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 10),
-                          Text(
-                            shoppingCart.quantity.toString(),
-                            style: const TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 16,
-                            ),
-                          ),
-                          const SizedBox(width: 10),
-                          InkWell(
-                            onTap: () {
-                              shoppingCartProvider
-                                  .addToCart(shoppingCart.product);
-                            },
-                            borderRadius: BorderRadius.circular(10.0),
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 10.0, vertical: 4.0),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10.0),
-                                color: Colors.grey.shade800,
-                              ),
-                              child: const Text(
-                                '+',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
+                return ItemCard(
+                  shoppingCart: shoppingCart,
+                  onTapImage: () {
+                    context
+                        .push('${Routes.product}/${shoppingCart.product.id}');
+                  },
+                  addToCart: () {
+                    shoppingCartProvider.addToCart(shoppingCart.product);
+                  },
+                  reduceCartQuantity: () {
+                    shoppingCartProvider
+                        .reduceCartQuantity(shoppingCart.product.id);
+                  },
                 );
               },
             ),
           ),
-          //
+
           // total section
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
             color: Colors.white,
             child: Column(
               children: [
-                // subtotal
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      "Subtotal",
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
+                TextCard(
+                  title: "Subtotal",
+                  subTitle:
                       '\$${shoppingCartProvider.total().toStringAsFixed(2)}',
-                      style: const TextStyle(
-                        color: Colors.grey,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
+                  color: Colors.grey,
                 ),
-
-                // delivery
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Delivery:",
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      'Free',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
+                const TextCard(
+                  title: "Delivery:",
+                  subTitle: 'Free',
+                  color: Colors.grey,
                 ),
-
                 const SizedBox(height: 5),
-                // total
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      "Total:",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18,
-                      ),
-                    ),
-                    Text(
+                TextCard(
+                  title: "Total:",
+                  subTitle:
                       '\$${shoppingCartProvider.total().toStringAsFixed(2)}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
+                  fontSize: 18,
                 ),
                 const SizedBox(height: 20),
+
                 // checkout button
                 MaterialButton(
-                  onPressed: () {},
+                  onPressed: orderProvider.isLoading || shoppingCartList.isEmpty
+                      ? null
+                      : () async {
+                          try {
+                            await orderProvider.makePayment(shoppingCartList);
+                            displayPaymentSheet(
+                                orderProvider, shoppingCartProvider);
+                          } catch (e) {
+                            if (e is StripeConfigException) {
+                              snackBar(context, "Payment Error");
+                            } else {
+                              snackBar(context,
+                                  "An error occurred, try again later.");
+                            }
+                          }
+                        },
                   elevation: 0,
                   minWidth: double.infinity,
+                  disabledColor: Colors.grey,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(12),
                   ),
-                  color: Colors.grey.shade900,
-                  child: const Padding(
-                    padding: EdgeInsets.all(16.0),
+                  color: const Color(0xff1d242d),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
                     child: Text(
-                      'Checkout',
-                      style: TextStyle(
+                      orderProvider.isLoading ? 'Loading...' : 'Checkout',
+                      style: const TextStyle(
                         color: Colors.white,
                         fontWeight: FontWeight.w600,
                       ),
@@ -256,6 +122,29 @@ class ShoppingCartScreen extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  Future<void> displayPaymentSheet(
+      OrderProvider orderProvider, ShoppingCartProvider cartProvider) async {
+    try {
+      // "Display payment sheet";
+      await Stripe.instance.presentPaymentSheet();
+      await orderProvider.createOrderPayment(cartProvider.shoppingCart);
+      cartProvider.clearCart();
+      // Show when payment is done, Displaying snackbar for it
+      snackBar(context, "Paid successfully");
+    } on StripeException catch (_) {
+      // If any error comes during payment, so payment will be cancelled
+      snackBar(context, " Payment Cancelled");
+    } catch (e) {
+      snackBar(context, "An error occurred, try again later.");
+    }
+  }
+
+  snackBar(BuildContext context, String message) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
     );
   }
 }
