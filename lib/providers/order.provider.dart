98b1e4facbd4c5f0bcd4d6dccee80358c1a427c0
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2024-07-09 19:17:21 -0400

    feat: add make-payment function

--- lib/providers/order.provider.dart
+++ lib/providers/order.provider.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+
+import 'package:flutter_stripe/flutter_stripe.dart';
+
 import 'package:app_belibeli/models/models.dart';
 import 'package:app_belibeli/services/services.dart';
 
@@ -10,19 +13,39 @@ class OrderProvider extends ChangeNotifier {
 
   bool get isLoading => _isLoading;
 
-  Future<Map<String, dynamic>> createPayment(CreateOrder dataPayment) async {
+  Future<void> makePayment(List<ShoppingCart> shoppingCart) async {
     _isLoading = true;
     notifyListeners();
-    final response = await _orderService.createPayment(dataPayment);
+    final response = await _orderService.createPayment(shoppingCart);
+
+    await Stripe.instance.initPaymentSheet(
+      paymentSheetParameters: SetupPaymentSheetParameters(
+        // Client secret key from payment data
+        paymentIntentClientSecret: response['client_secret'],
+        style: ThemeMode.light,
+        googlePay: const PaymentSheetGooglePay(
+          testEnv: true,
+          currencyCode: "USD",
+          merchantCountryCode: "US",
+        ),
+        merchantDisplayName: 'Flutter Ecommerce BeliBeli',
+        returnURL: 'flutterstripe://redirect',
+      ),
+    );
     _isLoading = false;
     notifyListeners();
-    return response;
   }
 
-  Future<Map<String, dynamic>> createOrderPayment(CreateOrder dataOrder) async {
+  Future<Map<String, dynamic>> createPayment(
+      List<ShoppingCart> shoppingCart) async {
+    return _orderService.createPayment(shoppingCart);
+  }
+
+  Future<Map<String, dynamic>> createOrderPayment(
+      List<ShoppingCart> shoppingCart) async {
     _isLoading = true;
     notifyListeners();
-    final response = await _orderService.createOrderPayment(dataOrder);
+    final response = await _orderService.createOrderPayment(shoppingCart);
     _isLoading = false;
     notifyListeners();
     return response;
